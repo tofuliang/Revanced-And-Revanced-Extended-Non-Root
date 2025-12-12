47dae7771aea67ed7a9ffdb02b21b5bb4a1110c9
tofuliang <tofuliang@gmail.com>
2025-12-12 23:28:38 +0800

    feat: add Solid-Explorer arm-v7a

--- src/build/scrazzz-revanced.sh
+++ src/build/scrazzz-revanced.sh
@@ -6,7 +6,13 @@ source ./src/build/utils.sh
 dl_gh "revanced-cli" "revanced" "latest"
 dl_gh "my-revanced-patches" "scrazzz" "prerelease"
 #################################################
-# Patch Soild Explorer arm64-v8a:
-get_patches_key "Soild-Explorer"
+# Patch Solid Explorer arm64-v8a:
+get_patches_key "Solid-Explorer"
+version="2.8.63"
 get_apk "pl.solidexplorer2" "solid-explorer-arm64-v8a" "solid-explorer-beta" "neatbytes/solid-explorer-beta/solid-explorer-file-manager" "arm64-v8a"
 patch "solid-explorer-arm64-v8a" "scrazzz"
+# Patch Solid Explorer armeabi-v7a:
+version="2.8.63"
+get_patches_key "Solid-Explorer"
+get_apk "pl.solidexplorer2" "solid-explorer-armeabi-v7a" "solid-explorer-beta" "neatbytes/solid-explorer-beta/solid-explorer-file-manager" "armeabi-v7a"
+patch "solid-explorer-armeabi-v7a" "scrazzz"
